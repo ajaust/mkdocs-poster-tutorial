585b7e8e6c391d94d5c387b9a0ea3b41b0909884
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2020-07-28 23:16:17 +0200

    first step set up

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,11 @@
+window.MathJax = {
+  loader: {load: ['[tex]/newcommand']},
+  tex: {packages: {'[+]': ['newcommand']}}
+  tex2jax: {
+    inlineMath: [ ["\\(","\\)"] ],
+    displayMath: [ ["\\[","\\]"] ]
+  },
+  displayAlign: "center",
+  showProcessingMessages: false,
+  messageStyle: "none"
+};
